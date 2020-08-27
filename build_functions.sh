059611126ed5d93adc9c8bcb880ebd5db4fb2185
Joshua B <joshdev@9ci.com>
2020-08-27 12:49:17 -0600

    add echo logging for circle debug

--- build_functions.sh
+++ build_functions.sh
@@ -5,6 +5,7 @@
 # source build/bin/build_functions.sh
 # ---
 binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "sourcing from binDir $binDir"
 source ${binDir}/setVar
 source ${binDir}/versions
 source ${binDir}/yaml
